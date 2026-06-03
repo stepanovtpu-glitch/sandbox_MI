4f607fb161f028542398c2b6332e716062c2c1dc
Alex Solve <stepanovtpu@gmail.com>
2026-06-03 21:44:11 +0800

    style: import document integrity stylesheet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './document.css';
 import './history.css';
+import './integrity.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
