35ca1122862acb8465a56d6604b991fdda2642ab
Alex Solve <stepanovtpu@gmail.com>
2026-06-03 21:33:04 +0800

    style: import history stylesheet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './document.css';
+import './history.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
