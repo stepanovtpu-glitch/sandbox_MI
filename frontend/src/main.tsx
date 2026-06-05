cbc5eb8a694c0605c4960030e54bd9af99a55c8d
Alex Solve <stepanovtpu@gmail.com>
2026-06-05 19:09:17 +0800

    style: import screen form stylesheet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import './document.css';
 import './history.css';
 import './integrity.css';
 import './system-status.css';
+import './screen-form.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
