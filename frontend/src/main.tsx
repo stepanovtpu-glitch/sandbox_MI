23c2a772f2a2ef2d5a20e1f5431011713027d656
Alex Solve <stepanovtpu@gmail.com>
2026-06-05 18:45:38 +0800

    style: import system status stylesheet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css';
 import './document.css';
 import './history.css';
 import './integrity.css';
+import './system-status.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
