40ad0f8c29c0b42b81d6054b68a3910aa07d8abc
Alex Solve <stepanovtpu@gmail.com>
2026-06-06 10:50:51 +0800

    style: import full history screen stylesheet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import './history.css';
 import './integrity.css';
 import './system-status.css';
 import './screen-form.css';
+import './history-screen.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
